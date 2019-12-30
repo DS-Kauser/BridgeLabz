3ab585ee35c4d820782b819040f1e831c04769af
DS-Kauser <kauserbest@gmail.com>
2019-12-30 17:42:12 +0530

    [Kauser]refractor. refracted the code with user input

--- Week1/Algorithm/merge_sort.py
+++ Week1/Algorithm/merge_sort.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('/home/user/Desktop/Mack/BridgeLabz')         # helps in importing function from utility
+sys.path.append('/home/user/Desktop/BridgeLabz')         # helps in importing function from utility
 
 # importing merge_sort from utility
 from Week1.Utility.utility import merge_sort                             
@@ -7,7 +7,12 @@ from Week1.Utility.utility import merge_sort
 if __name__ == '__main__':                                          
     try:
         #taking a list and sorting it using merge sort                                                             
-        alist = [5,1,2,6,4,3,10,7,9,8,12,16,13]                     
+        num_of_element = int(input("Enter number of element in your list : "))
+        alist = []
+        for i in range(num_of_element):
+            temp = int(input("Enter your number : "))
+            alist.append(temp)
+                             
         alist = merge_sort(alist)                                   
         print(alist)
     
