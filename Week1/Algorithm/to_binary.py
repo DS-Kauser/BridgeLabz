013fecb3278f663939b8d5fc166ed37bbf9199bc
DS-Kauser <kauserbest@gmail.com>
2019-12-30 18:02:34 +0530

    [Kauser]Refractor. added discription in print function

--- Week1/Algorithm/to_binary.py
+++ Week1/Algorithm/to_binary.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('/home/user/Desktop/Mack/BridgeLabz')                 # helps in importing function from utility
+sys.path.append('/home/user/Desktop/BridgeLabz')                 # helps in importing function from utility
 
 # importing to_binary function
 from Week1.Utility.utility import to_binary                                       
@@ -10,7 +10,7 @@ if __name__ == '__main__':
             # taking decimal number and converting it into binary
             num = int(input("Enter any decimal number : "))                 
             binary = to_binary(num)                                         
-            print(binary)
+            print("Binary conversion for {0} is : {1}".format(num, binary))
             
             # breaking the loop
             break                                                           
