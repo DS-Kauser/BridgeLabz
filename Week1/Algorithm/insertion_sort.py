d4ea3be88e4f89bdf9d33a529caca44e1c61d185
DS-Kauser <kauserbest@gmail.com>
2019-12-30 17:40:34 +0530

    [Kauser]refractor. refracted code with user input list

--- Week1/Algorithm/insertion_sort.py
+++ Week1/Algorithm/insertion_sort.py
@@ -8,8 +8,12 @@ if __name__ == '__main__':
     
     try:
         # taking list and sorting it using insertion sort                                                            
-        
-        alist = [5,1,2,6,4,3,10,7,9,8]                              
+        num_of_element = int(input("Enter number of element in your list : "))
+        alist = []
+        for i in range(num_of_element):
+            temp = int(input("Enter your number : "))
+            alist.append(temp)
+                                      
         alist = insertion_sort(alist)                               
         print(alist)                                                
 
