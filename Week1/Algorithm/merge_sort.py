ee73d4bd87e3019aa70a00d1a17df7737a05ed87
DS-Kauser <kauserbest@gmail.com>
2019-12-30 12:27:12 +0530

    [Kauser]update. updated module location

--- Week1/Algorithm/merge_sort.py
+++ Week1/Algorithm/merge_sort.py
@@ -1,8 +1,8 @@
 import sys
-sys.path.append('/home/user/Desktop/Mack/BridgeLabz/Week1')         # helps in importing function from utility
+sys.path.append('/home/user/Desktop/Mack/BridgeLabz')         # helps in importing function from utility
 
 # importing merge_sort from utility
-from Utility.utility import merge_sort                             
+from Week1.Utility.utility import merge_sort                             
 
 if __name__ == '__main__':                                          
     try:
