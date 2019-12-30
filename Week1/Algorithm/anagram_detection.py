e248202d28fdd46679d3d0d85a4061a80c9aeefb
DS-Kauser <kauserbest@gmail.com>
2019-12-30 12:19:12 +0530

    [Kauser]refractor. refracted this code

--- Week1/Algorithm/anagram_detection.py
+++ Week1/Algorithm/anagram_detection.py
@@ -1,14 +1,14 @@
 import sys                                                          
-sys.path.append('/home/user/Desktop/Mack/BridgeLabz/Week1')             # help in importing function from utility
+sys.path.append('/home/user/Desktop/BridgeLabz')             # help in importing function from utility
 
-from Utility.utility import anagram_detection                           
+from Week1.Utility.utility import anagram_detection                           
 
 if __name__ == '__main__':                                              
     while True:                                                         
         try:                                                            
             # taking two user input
-            first_word = input("Enter first word : ")                   
-            second_word = input("Enter second word : ")                 
+            first_word = str(input("Enter first word : "))                   
+            second_word = str(input("Enter second word : "))
             
             # checking anagram
             anagram_detection(first_word, second_word)                  
