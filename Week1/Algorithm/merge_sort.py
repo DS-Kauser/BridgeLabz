f069e01e33c54735f306343e28250825357db07f
DS-Kauser <kauserbest@gmail.com>
2019-12-30 17:44:13 +0530

    [Kauser]refractor. run again if error occurs

--- Week1/Algorithm/merge_sort.py
+++ Week1/Algorithm/merge_sort.py
@@ -5,16 +5,18 @@ sys.path.append('/home/user/Desktop/BridgeLabz')         # helps in importing fu
 from Week1.Utility.utility import merge_sort                             
 
 if __name__ == '__main__':                                          
-    try:
-        #taking a list and sorting it using merge sort                                                             
-        num_of_element = int(input("Enter number of element in your list : "))
-        alist = []
-        for i in range(num_of_element):
-            temp = int(input("Enter your number : "))
-            alist.append(temp)
-                             
-        alist = merge_sort(alist)                                   
-        print(alist)
-    
-    except:
-        print("oops! something wet wrong")                          
+    while True:
+        try:
+            #taking a list and sorting it using merge sort                                                             
+            num_of_element = int(input("Enter number of element in your list : "))
+            alist = []
+            for i in range(num_of_element):
+                temp = int(input("Enter your number : "))
+                alist.append(temp)
+                                
+            alist = merge_sort(alist)                                   
+            print(alist)
+            break
+        
+        except:
+            print("oops! something wet wrong")                          
