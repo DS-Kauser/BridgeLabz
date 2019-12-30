a068688e9bba0a61a18a4ee9814946f3b195daf0
DS-Kauser <kauserbest@gmail.com>
2019-12-30 18:07:11 +0530

    [Kauser]Refractor. change module location

--- Week1/Function/Tic_tac_toe.py
+++ Week1/Function/Tic_tac_toe.py
@@ -1,7 +1,7 @@
 import sys
-sys.path.append('/home/user/Desktop/Mack/BridgeLabz/Week1')         # helps in importing function from utility 
+sys.path.append('/home/user/Desktop/BridgeLabz')         # helps in importing function from utility 
 
-from Utility.utility import play_tic_tac_toe                        # importing play_tic_tac_toe function from utility
+from Week1.Utility.utility import play_tic_tac_toe                        # importing play_tic_tac_toe function from utility
 
 if __name__ == '__main__':                                          # cresting main method
     while True:                                                     # to run the code again if any error is found
