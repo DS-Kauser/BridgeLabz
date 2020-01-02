ed2ee165827e1224d24e37c422af901aa7273832
DS-Kauser <kauserbest@gmail.com>
2020-01-02 10:35:00 +0530

    [Kauser]Update. added class node and linked_list

--- Week2/Utility/utility.py
+++ Week2/Utility/utility.py
@@ -1,6 +1,6 @@
 """
  ******************************************************************************
- *  Purpose: utils for functions programs and algorithms
+ *  Purpose: utils for data structure program
  *
  *  @author  Md Kauser Ansari
  *  @version 3.7
@@ -8,3 +8,21 @@
  ******************************************************************************
 """
 
+"""
+adding a node class
+creates node with data and refrence to next node
+"""
+class node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+"""
+creating a class linked_list
+adding different method to it
+"""
+
+class linked_list:
+    def __init__(self):
+        self.head = None
