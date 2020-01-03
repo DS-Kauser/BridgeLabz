e1c275e1fc4e3776d3ff09b71448345c7c52df0f
DS-Kauser <kauserbest@gmail.com>
2020-01-03 19:35:01 +0530

    [Kauser]Initial.

---
+++ Week2/Program/pelindrome_checker.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append("/home/user/Desktop/BridgeLabz")
+
+from Week2.Utility.utility import deque
+
+word = input("Enter a word to check : ")
+deque_list = deque()
+for i in word:
+    
