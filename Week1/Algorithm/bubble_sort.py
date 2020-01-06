21d8c4c1b1674dfbe4ca3d02959e3b4b96869ad8
DS-Kauser <kauserbest@gmail.com>
2020-01-06 17:53:16 +0530

    [Kauser]Refractor. added bubble_sort_list method and main method in bubble_sort.py

--- Week1/Algorithm/bubble_sort.py
+++ Week1/Algorithm/bubble_sort.py
@@ -1,21 +1,16 @@
 import sys
 sys.path.append('/home/user/Desktop/BridgeLabz')             # helps in importing function from utility
-
 # importing bubble_sort function
 from Week1.Utility.utility import bubble_sort                                 
 
+def bubble_sort_list():
+    num_of_element = int(input("Enter number of element in the list : "))
+    a_list = []
+    for i in range(num_of_element):
+        temp = int(input("Enter your integers : "))
+        a_list.append(temp)
+    a_list = bubble_sort(a_list)                                  
+    print(a_list)
+
 if __name__ == '__main__':                                              
-    while True:                                                         
-        try:                                                            
-            #sorting with bubble sort
-            num_of_element = int(input("Enter number of element in the list : "))
-            a_list = []
-            for i in range(num_of_element):
-                temp = int(input("Enter your integers : "))
-                a_list.append(temp)
-                              
-            a_list = bubble_sort(a_list)                                  
-            print(a_list)
-            break
-        except:                                                         
-            print("Invalid Input")                                      
+    bubble_sort_list()
