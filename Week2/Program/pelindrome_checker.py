d4666e4fb2387d1d32ce74cda8109a71acb78196
DS-Kauser <kauserbest@gmail.com>
2020-01-06 10:00:21 +0530

    [Kauser]Refractor. refractor the code

--- Week2/Program/pelindrome_checker.py
+++ Week2/Program/pelindrome_checker.py
@@ -3,7 +3,26 @@ sys.path.append("/home/user/Desktop/BridgeLabz")
 
 from Week2.Utility.utility import deque
 
-word = input("Enter a word to check : ")
-deque_list = deque()
-for i in word:
-    
+def check_pelindrome(word):
+    try:
+        
+        deque_list = deque()
+        for i in word:
+            deque_list.add_rear(i)
+
+        deque_word = ''
+        cur_node = deque_list.head
+        while cur_node is not None:
+            deque_word = deque_word + cur_node.data
+            cur_node = cur_node.next
+
+        if word == deque_word:
+            print("Given input {0} is pelindrome".format(word))
+        else:
+            print("Given input {0} is not pelindrome".format(word))
+    except:
+        print("Invalid input")
+
+if __name__ == "__main__":
+    word = input("Enter a word to check : ")
+    check_pelindrome(word)
