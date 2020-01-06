8dcbb4ce86b83e7912dd140efb54622f604421e8
DS-Kauser <kauserbest@gmail.com>
2020-01-06 14:14:19 +0530

    [Kauser]Initial.

---
+++ Week2/Program/prime_stack.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append("/home/user/Desktop/BridgeLabz")
+
+
+from Week2.Utility.utility import find_anagram
+from Week2.Utility.utility import prime
+from Week2.Utility.utility import stack
