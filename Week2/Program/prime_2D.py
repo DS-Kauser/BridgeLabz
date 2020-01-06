958976952216d16e8a3c14f5795fb3fae5b30352
DS-Kauser <kauserbest@gmail.com>
2020-01-06 12:56:43 +0530

    [Kauser]Refractor. added main method

--- Week2/Program/prime_2D.py
+++ Week2/Program/prime_2D.py
@@ -3,10 +3,16 @@ sys.path.append("/home/user/Desktop/BridgeLabz")
 
 from Week2.Utility.utility import prime
 
-array = []
-for i in range(10):
-    array[i] = []
+def prime_2D():
+    array = []
+    start = 0
+    end = 100
+    for i in range(10):
+        array.append(prime(start,end))
+        start += 100
+        end += 100
+    return array
 
-start = int(input("Enter your start point number : "))
-end = int(input("Enter your end point number : "))
-
+if __name__ == "__main__":
+    arr = prime_2D()
+    print(arr)
