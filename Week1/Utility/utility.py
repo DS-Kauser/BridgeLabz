a3d48340723c4f119737221f50f08a3a62696652
DS-Kauser <kauserbest@gmail.com>
2020-01-06 17:14:44 +0530

    [Kauser]Refractor. Refractor find_leap_year method in utility.py

--- Week1/Utility/utility.py
+++ Week1/Utility/utility.py
@@ -332,21 +332,18 @@ check for century and leap year
 """
 def find_leap_year(year):                           
     if len(str(year)) > 3:                                              
-
         if year % 4 == 0:                                               
             if year % 100 == 0:                                         
                 if year % 400 == 0:                                     
                     print("{0} is a leap year".format(year))            
-                else:
-                    print("{0} is not a leap year".format(year))        
-            else:
-                print("{0} is a leap year".format(year))                
-
-        else:
-            print("{0} is not a leap year".format(year))                
-
-    else:
-        print("Enter a valid year of four digits like '1995'")          
+                    return 
+                print("{0} is not a leap year".format(year))        
+                return
+            print("{0} is a leap year".format(year))                
+            return 
+        print("{0} is not a leap year".format(year))                
+        return
+    print("Enter a valid year of four digits like '1995'")          
 
 
 # Show Array
