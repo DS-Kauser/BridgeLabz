5306a4f5a2340cb0fa47f8347728fcb360ff6eb1
DS-Kauser <kauserbest@gmail.com>
2020-01-06 17:49:47 +0530

    [Kauser]Refractor. added method detect_anagram and main method in anagram_detection.py

--- Week1/Algorithm/anagram_detection.py
+++ Week1/Algorithm/anagram_detection.py
@@ -1,18 +1,16 @@
 import sys                                                          
 sys.path.append('/home/user/Desktop/BridgeLabz')             # help in importing function from utility
-
 from Week1.Utility.utility import anagram_detection                           
 
-if __name__ == '__main__':                                              
-    while True:                                                         
-        try:                                                            
-            # taking two user input
-            first_word = str(input("Enter first word : "))                   
-            second_word = str(input("Enter second word : "))
-            
-            # checking anagram
-            anagram_detection(first_word, second_word)                  
-            break                                                       
+def detect_anagram():
+    try:                                                            
+        # taking two user input
+        first_word = str(input("Enter first word : "))                   
+        second_word = str(input("Enter second word : "))
+        # checking anagram
+        anagram_detection(first_word, second_word)                                                                         
+    except:                                                         
+        print("Enter a valid input")    
 
-        except:                                                         
-            print("Enter a valid input")                                
+if __name__ == '__main__':                                              
+    detect_anagram()                                
