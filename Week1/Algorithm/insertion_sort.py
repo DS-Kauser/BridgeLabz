318a60dca14c3b78ce9063cb65a13b46f105a62a
DS-Kauser <kauserbest@gmail.com>
2020-01-06 18:00:35 +0530

    [Kauser]Refractor. added insertion_sort_list method and main method in insertion_sort.py

--- Week1/Algorithm/insertion_sort.py
+++ Week1/Algorithm/insertion_sort.py
@@ -1,11 +1,9 @@
 import sys
 sys.path.append('/home/user/Desktop/BridgeLabz')         # helps in importing function from utility
-
 # importing insertion_sort function
 from Week1.Utility.utility import insertion_sort                          
-
-if __name__ == '__main__':                                          
-    
+                                          
+def insertion_sort_list():    
     try:
         # taking list and sorting it using insertion sort                                                            
         num_of_element = int(input("Enter number of element in your list : "))
@@ -13,9 +11,10 @@ if __name__ == '__main__':
         for i in range(num_of_element):
             temp = int(input("Enter your number : "))
             a_list.append(temp)
-                                      
         a_list = insertion_sort(a_list)                               
         print(a_list)                                                
-
     except:                                                         
         print("Please check your input")                         
+
+if __name__ == '__main__':
+    insertion_sort_list()
