5f01d85656166a50529d0dafeaa2c1a71830bf0d
DS-Kauser <kauserbest@gmail.com>
2020-01-06 12:31:08 +0530

    [Kauser]Initial.

---
+++ Week2/Program/prime_2D.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append("/home/user/Desktop/BridgeLabz")
+
+from Week2.Utility.utility import prime
+
+array = []
+for i in range(10):
+    array[i] = []
+
+start = int(input("Enter your start point number : "))
+end = int(input("Enter your end point number : "))
+
