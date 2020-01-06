e116a2950b16812d326bfad360672dcc04c18e1b
DS-Kauser <kauserbest@gmail.com>
2020-01-06 17:36:08 +0530

    [Kauser]Add. added gambling_win_lose.py file

---
+++ Week1/Function/gambling_win_lose.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('/home/user/Desktop/BridgeLabz')         # helps in importing function from utility
+
+# importing find_leap_year from utility
+from Week1.Utility.utility import gambler
+                         
