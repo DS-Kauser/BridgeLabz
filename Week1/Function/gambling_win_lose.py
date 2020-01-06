c663089a58a91de60cb42fe83f484e03c7c0fe54
DS-Kauser <kauserbest@gmail.com>
2020-01-06 17:42:59 +0530

    [Kauser]Refractor. Added start_gambling method and main method in gambling_win_lose.py

--- Week1/Function/gambling_win_lose.py
+++ Week1/Function/gambling_win_lose.py
@@ -3,4 +3,14 @@ sys.path.append('/home/user/Desktop/BridgeLabz')         # helps in importing fu
 
 # importing find_leap_year from utility
 from Week1.Utility.utility import gambler
-                         
+
+def start_gambling():
+    # taking user input
+    stake = int(input("Enter your stake : "))
+    goal = int(input("Enter your goal value : "))
+    trails = int(input("Enter number of trails : "))
+    gambler(stake, goal, trails)
+
+# adding main method
+if __name__ == "__main__":
+    start_gambling()
