d82451b04da680ff631773d30d6a5d0931e1abba
DS-Kauser <kauserbest@gmail.com>
2020-01-06 16:01:12 +0530

    [Kauser]Refractor. Refractor change to a_list object name in  binary_search method in utility.py

--- Week1/Algorithm/search_binary.py
+++ Week1/Algorithm/search_binary.py
@@ -11,18 +11,18 @@ if __name__ == '__main__':                                                  # cr
             # opening file and reading it
             file1 = open('text.txt', 'r')
             f1 = file1.readlines()
-            alist = []
+            a_list = []
             for line in f1:
                 word = line.split()
-                alist.extend(word)
+                a_list.extend(word)
             
-            alist = sorted(alist)                                           # sorting that list 
-            print("Sorted list : {0}".format(alist))
+            a_list = sorted(a_list)                                           # sorting that list 
+            print("Sorted list : {0}".format(a_list))
             start = 0
-            end = len(alist)-1
+            end = len(a_list)-1
             word = input("Enter word that should be searched : ")           # taking user input word to be searched
 
-            binary_search(alist, start, end, word)                          # searching that word in that list
+            binary_search(a_list, start, end, word)                          # searching that word in that list
             break
         except:
             print("Enter a valid input")                                    # if any error is found
