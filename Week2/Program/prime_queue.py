261db47877f4e30599b9fac2f902a3fe09db2756
DS-Kauser <kauserbest@gmail.com>
2020-01-06 14:23:46 +0530

    [Kauser]Refractor. added try catch block

--- Week2/Program/prime_queue.py
+++ Week2/Program/prime_queue.py
@@ -7,12 +7,15 @@ from Week2.Utility.utility import prime
 from Week2.Utility.utility import queue
 
 def queue_prime():
-    prime_queue = queue()
-    start = 0
-    end = 1000
-    for i in find_anagram(prime(start,end)):
-        prime_queue.enqueue(i)
-    prime_queue.print_list()
+    try:
+        prime_queue = queue()
+        start = 0
+        end = 1000
+        for i in find_anagram(prime(start,end)):
+            prime_queue.enqueue(i)
+        prime_queue.print_list()
+    except:
+        print("Check for index error")
 
 if __name__ == "__main__":
     queue_prime()
