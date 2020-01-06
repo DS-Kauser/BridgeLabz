762a91d1ec4bf64e944bb2e340b83ceaf4e55cd4
DS-Kauser <kauserbest@gmail.com>
2020-01-06 16:17:07 +0530

    [Kauser]Refractor. alist object to a_list in bubble_sort.py

--- Week1/Algorithm/bubble_sort.py
+++ Week1/Algorithm/bubble_sort.py
@@ -9,13 +9,13 @@ if __name__ == '__main__':
         try:                                                            
             #sorting with bubble sort
             num_of_element = int(input("Enter number of element in the list : "))
-            alist = []
+            a_list = []
             for i in range(num_of_element):
                 temp = int(input("Enter your integers : "))
-                alist.append(temp)
+                a_list.append(temp)
                               
-            alist = bubble_sort(alist)                                  
-            print(alist)
+            a_list = bubble_sort(a_list)                                  
+            print(a_list)
             break
         except:                                                         
             print("Invalid Input")                                      
