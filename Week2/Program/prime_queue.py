8bb22a3827a068be42cbee5bd932ccd77c98ab58
DS-Kauser <kauserbest@gmail.com>
2020-01-07 19:12:54 +0530

    [Kauser]Refractor. refractor queue_prime method prime_queue.py

--- Week2/Program/prime_queue.py
+++ Week2/Program/prime_queue.py
@@ -7,15 +7,12 @@ from Week2.Utility.utility import prime
 from Week2.Utility.utility import queue
 
 def queue_prime():
-    try:
-        prime_queue = queue()
-        start = 0
-        end = 1000
-        for i in find_anagram(prime(start,end)):
-            prime_queue.enqueue(i)
-        prime_queue.print_list()
-    except:
-        print("Check for index error")
+    prime_queue = queue()
+    start = 0
+    end = 1000
+    for i in find_anagram(prime(start,end)):
+        prime_queue.enqueue(i)
+    prime_queue.print_list()
 
 if __name__ == "__main__":
     queue_prime()
