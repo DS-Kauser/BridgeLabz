5186b1bc7d51c55f1b663d48985f01b002d1cb8f
DS-Kauser <kauserbest@gmail.com>
2020-01-07 19:33:53 +0530

    [Kauser]Refractor. added create_linked_list,add_remove, rewrite and main method in unordered_list.py

--- Week2/Program/unordered_list.py
+++ Week2/Program/unordered_list.py
@@ -1,42 +1,35 @@
 import sys
 sys.path.append("/home/user/Desktop/BridgeLabz")
+from Week2.Utility.utility import LinkedList
 
-from Week2.Utility.utility import node
-from Week2.Utility.utility import unord_linked_list
+def create_linked_list(file_path):
+    open_file = open(file_path, 'r')
+    lst = LinkedList()
+    for line in open_file:
+        for word in line.split():
+            lst.insert_end(word)
+    open_file.close()
+    return lst
 
+def add_remove(word, lst):
+    position = lst.pos(word)
+    if position is not None:
+        lst.del_at(position)
+        return lst
+    lst.insert_end(word)
+    return lst
+
+def rewrite(file_path, lst):
+    open_file = open(file_path,'w')
+    cur_node = lst.head
+    while cur_node is not None:
+        open_file.write("{0}\n".format(cur_node.data))
+        cur_node = cur_node.next
+    return
 
 if __name__ == '__main__':
-    while True:
-        try:
-            file_path = '/home/user/Desktop/BridgeLabz/Week2/Utility/text_unordered.txt'
-
-            open_file = open(file_path, 'r')
-
-            list1 = unord_linked_list()
-            for line in open_file:
-                for word in line.split():
-                    list1.insert_end(word)
-            open_file.close()
-            #l1.print_list()
-            word = input("Enter the word to search : ")
-            word_pos = list1.pos(word)
-
-            if word_pos is None:
-                list1.insert_end(word)
-                filew = open(file_path, 'a')
-                filew.write('{0}\n'.format(word))
-
-            else:
-                list1.del_at(word_pos)
-                open_file = open(file_path, 'w')
-                cur_node = list1.head
-                while True:
-                    if cur_node is None:
-                        open_file.close()
-                        break
-                    open_file.write('{0}\n'.format(cur_node.data))
-                    cur_node = cur_node.next
-            break
-        
-        except:
-            print("Enter a valid input")
+    file_path = '/home/user/Desktop/BridgeLabz/Week2/Utility/text_unordered.txt'
+    word = input("Enter a word to search : ")
+    lst = create_linked_list(file_path)
+    lst = add_remove(word, lst)
+    rewrite(file_path, lst)
