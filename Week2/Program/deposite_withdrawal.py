ac1ccb8cccb4310d8b013e7ebaebe1d02d9b1f6e
DS-Kauser <kauserbest@gmail.com>
2020-01-07 10:36:28 +0530

    [Kauser]Refractor. added bank_counter and main method in deposite_withdrawal.py

--- Week2/Program/deposite_withdrawal.py
+++ Week2/Program/deposite_withdrawal.py
@@ -1,19 +1,16 @@
 import sys
 sys.path.append("/home/user/Desktop/BridgeLabz")
-
 from Week2.Utility.utility import queue
 
 #Suppose minimum bank balance is Rs.10000
 
-if __name__ == "__main__":
+def bank_counter():
     while True:
         try:
             num_of_people = int(input("Enter number of people in queue : "))
             bank_que = queue()
-            
             for i in range(num_of_people):
                 bank_que.enqueue(i)
-
             while bank_que.is_empty() is not True:
                 file_path = '/home/user/Desktop/BridgeLabz/Week2/Utility/balance.txt'
                 file_op = open(file_path, 'r')
@@ -26,42 +23,35 @@ if __name__ == "__main__":
                 
                 if option == 1:
                     dep_amount = int(input("Enter your amount : "))
-                    
                     if dep_amount >= 0:
                         file_op = open(file_path, 'w')
                         file_op.write("{0}".format(dep_amount+bal))
                         file_op.close()
                         bank_que.dequeue()
-                    
-                    else:
-                        print("Enter a valid amount")
                         continue
+                    print("Enter a valid amount")
+                    continue
 
-                elif option == 2:
+                if option == 2:
                     with_amount = int(input("Enter your withdrawal amount : "))
-                    
                     if with_amount >= 0:
                         min_bal = 10000
-                        
                         if bal-min_bal >= with_amount:
                             bal = bal - with_amount
                             file_op = open(file_path, 'w')
                             file_op.write('{0}'.format(bal))
                             file_op.close() 
                             bank_que.dequeue()
-                        
-                        else:
-                            print("Empty Balance")
-                            break
-                    
-                    else:
-                        print("Enter a valid amount")
-                        continue
+                            continue
+                        print("Empty Balance")
+                        break                  
+                    print("Enter a valid amount")
+                    continue
                 
-                else:
-                    print("\nChoose Wisely")
-            
+                print("\nChoose Wisely")
             break
-
         except:
             print("Enter valid input")
+
+if __name__ == "__main__":
+    bank_counter()    
