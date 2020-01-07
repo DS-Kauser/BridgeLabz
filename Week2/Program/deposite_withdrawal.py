50c26479468be03011744f7ac4dc999105da10db
DS-Kauser <kauserbest@gmail.com>
2020-01-07 10:56:47 +0530

    [Kauser]Refractor. added make_queue, choice and main method in deposite_withdrawal.py

--- Week2/Program/deposite_withdrawal.py
+++ Week2/Program/deposite_withdrawal.py
@@ -4,13 +4,50 @@ from Week2.Utility.utility import queue
 
 #Suppose minimum bank balance is Rs.10000
 
+def make_queue(num_of_people):
+    bank_que = queue()
+    for i in range(num_of_people):
+        bank_que.enqueue(i)
+    return bank_que
+
+def choice(file_path, bal, option):
+    transaction = None
+    if option == 1:
+        dep_amount = int(input("Enter your amount : "))
+        if dep_amount >= 0:
+            file_op = open(file_path, 'w')
+            file_op.write("{0}".format(dep_amount+bal))
+            file_op.close()
+            transaction = True
+            return transaction
+        print("Enter a valid amount")
+        return transaction
+    
+    if option == 2:
+        with_amount = int(input("Enter your withdrawal amount : "))
+        if with_amount >= 0:
+            min_bal = 10000
+            if bal-min_bal >= with_amount:
+                bal = bal - with_amount
+                file_op = open(file_path, 'w')
+                file_op.write('{0}'.format(bal))
+                file_op.close() 
+                transaction = True
+                return transaction
+                #bank_que.dequeue()
+            print("Empty Balance")
+            return transaction
+        print("Enter a valid amount")
+        return transaction
+    print("\nChoose Wisely")
+    return transaction
+
 def bank_counter():
     while True:
         try:
             num_of_people = int(input("Enter number of people in queue : "))
-            bank_que = queue()
-            for i in range(num_of_people):
-                bank_que.enqueue(i)
+            bank_que = make_queue(num_of_people)
+            
             while bank_que.is_empty() is not True:
                 file_path = '/home/user/Desktop/BridgeLabz/Week2/Utility/balance.txt'
                 file_op = open(file_path, 'r')
@@ -21,34 +58,9 @@ def bank_counter():
                 print("choose\n1. Deposite\n2. Withdrawal")
                 option = int(input("Enter your choice : "))
                 
-                if option == 1:
-                    dep_amount = int(input("Enter your amount : "))
-                    if dep_amount >= 0:
-                        file_op = open(file_path, 'w')
-                        file_op.write("{0}".format(dep_amount+bal))
-                        file_op.close()
-                        bank_que.dequeue()
-                        continue
-                    print("Enter a valid amount")
-                    continue
-
-                if option == 2:
-                    with_amount = int(input("Enter your withdrawal amount : "))
-                    if with_amount >= 0:
-                        min_bal = 10000
-                        if bal-min_bal >= with_amount:
-                            bal = bal - with_amount
-                            file_op = open(file_path, 'w')
-                            file_op.write('{0}'.format(bal))
-                            file_op.close() 
-                            bank_que.dequeue()
-                            continue
-                        print("Empty Balance")
-                        break                  
-                    print("Enter a valid amount")
-                    continue
-                
-                print("\nChoose Wisely")
+                transaction = choice(file_path, bal, option)
+                if transaction == True:
+                    bank_que.dequeue()   
             break
         except:
             print("Enter valid input")
