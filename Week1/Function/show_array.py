9ae125a1640de7e91a29e86abdade7624474c48c
DS-Kauser <kauserbest@gmail.com>
2020-01-07 10:01:51 +0530

    [Kauser]Refractor. added srray_2D and main method in show_array.py

--- Week1/Function/show_array.py
+++ Week1/Function/show_array.py
@@ -1,25 +1,21 @@
 import sys
 sys.path.append('/home/user/Desktop/BridgeLabz')             # helps in importing function from utility
-
 # importing show_array from utility
 from Week1.Utility.utility import show_array                                  
 
-if __name__ == '__main__':                                              
-    
-    while True:                                                         
-        try:
-            # taking user-input for rows and columns                                                            
-            row = int(input("Enter number of rows : "))                 
-            col = int(input("Enter number of columns : "))              
-            
-            # passing the argument in show_array function
-            
-            array2D = show_array(row, col)                               
-            print("Your 2D array is \n")
-            print(array2D)                                      
-            
-            # breaking while loop
-            break                                                       
+def array_2D():
+    try:
+        # taking user-input for rows and columns                                                            
+        row = int(input("Enter number of rows : "))                 
+        col = int(input("Enter number of columns : "))              
+        
+        # passing the argument in show_array function
+        array2D = show_array(row, col)                               
+        print("Your 2D array is: \n")
+        print(array2D)                                      
+    except:
+        print("Enter Valid Input")                                  
 
-        except:
-            print("Enter Valid Input")                                  
+if __name__ == '__main__':                                              
+    array_2D()                                      
+        
