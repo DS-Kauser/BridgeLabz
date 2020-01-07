eec9c213e2f2626f0fae6c4bcd14db5b67c28a15
DS-Kauser <kauserbest@gmail.com>
2020-01-07 09:20:35 +0530

    [Kauser]Refractor. added merge_sort_list and main method in merge_sort.py

--- Week1/Algorithm/merge_sort.py
+++ Week1/Algorithm/merge_sort.py
@@ -1,22 +1,23 @@
 import sys
 sys.path.append('/home/user/Desktop/BridgeLabz')         # helps in importing function from utility
-
 # importing merge_sort from utility
 from Week1.Utility.utility import merge_sort                             
 
+def merge_sort_list():
+    #try:
+        #taking a list and sorting it using merge sort                                                             
+        num_of_element = int(input("Enter number of element in your list : "))
+        alist = []
+        for i in range(num_of_element):
+            temp = int(input("Enter your number : "))
+            alist.append(temp)
+        start = 0
+        end = len(alist)
+        alist = merge_sort(alist, start, end)                                   
+        return alist
+    #except:
+        #print("Check your input")
+
+# adding a main method
 if __name__ == '__main__':                                          
-    while True:
-        try:
-            #taking a list and sorting it using merge sort                                                             
-            num_of_element = int(input("Enter number of element in your list : "))
-            alist = []
-            for i in range(num_of_element):
-                temp = int(input("Enter your number : "))
-                alist.append(temp)
-                                
-            alist = merge_sort(alist)                                   
-            print(alist)
-            break
-        
-        except:
-            print("oops! something wet wrong")                          
+    print(merge_sort_list())
