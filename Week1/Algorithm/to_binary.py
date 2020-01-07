1a5ead3154eb8509a4cd9283d2729440bea33ec5
DS-Kauser <kauserbest@gmail.com>
2020-01-07 09:35:15 +0530

    [Kauser]Refractor. added get_binary and main method in to_binary.py

--- Week1/Algorithm/to_binary.py
+++ Week1/Algorithm/to_binary.py
@@ -1,18 +1,16 @@
 import sys
 sys.path.append('/home/user/Desktop/BridgeLabz')                 # helps in importing function from utility
-
 # importing to_binary function
 from Week1.Utility.utility import to_binary                                       
 
+def get_binary():
+    try:                                                                
+        # taking decimal number and converting it into binary
+        num = int(input("Enter any decimal number : "))                 
+        binary = to_binary(num)                                         
+        print("Binary conversion for {0} is : {1}".format(num, binary))                                                          
+    except:
+        print("Please Enter a valid input")                             
+
 if __name__ == '__main__':                                                  
-    while True:                                                             
-        try:                                                                
-            # taking decimal number and converting it into binary
-            num = int(input("Enter any decimal number : "))                 
-            binary = to_binary(num)                                         
-            print("Binary conversion for {0} is : {1}".format(num, binary))
-            
-            # breaking the loop
-            break                                                           
-        except:
-            print("Please Enter a valid input")                             
+    get_binary()
