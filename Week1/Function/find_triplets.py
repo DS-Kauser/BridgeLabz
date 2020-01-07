d7d711f2b1a39321222c8b440eec2cba9124bf46
DS-Kauser <kauserbest@gmail.com>
2020-01-07 09:56:25 +0530

    [Kauser]Refractor. added tri_sum_zero and main method in find_triplets.py

--- Week1/Function/find_triplets.py
+++ Week1/Function/find_triplets.py
@@ -1,24 +1,23 @@
 import sys
 sys.path.append('/home/user/Desktop/BridgeLabz')                     # helps in importing function from utility
-
 # importing find_triplets from utility
 from Week1.Utility.utility import find_triplets                                       
 
-if __name__ == '__main__':                                                      
-    while True:                                                                 
-        try:         
-            # taking elements into list                                                           
-            num_of_element = int(input("Enter number of element in the list : "))
-            alist = []
-            for i in range(num_of_element):
-                temp = int(input("Enter your numbers : "))
-                alist.append(temp)      
-            
-            # passing list as argument to calculate triplets
-            triplet, num_of_trip = find_triplets(alist)                         
-            print("Triplets present : ", triplet)
-            print("Number of triplets : {0}".format(num_of_trip))
-            break                                                               
+def tri_sum_zero():
+    try:         
+        # taking elements into list                                                           
+        num_of_element = int(input("Enter number of element in the list : "))
+        alist = []
+        for i in range(num_of_element):
+            temp = int(input("Enter your numbers : "))
+            alist.append(temp)      
         
-        except:                                                         
-            print("Enter a valid input")                                        
+        # passing list as argument to calculate triplets
+        triplet, num_of_trip = find_triplets(alist)                         
+        print("Triplets present : ", triplet)
+        print("Number of triplets : {0}".format(num_of_trip))
+    except:                                                         
+        print("Enter a valid input")                                        
+
+if __name__ == '__main__':
+    tri_sum_zero()
