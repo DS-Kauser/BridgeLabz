224460da6de2352c435a58f4a646ca0774dfc52a
DS-Kauser <kauserbest@gmail.com>
2020-01-07 09:46:42 +0530

    [Kauser]Refractor. added min_change() and main method to change_money.py

--- Week1/Function/change_money.py
+++ Week1/Function/change_money.py
@@ -1,24 +1,21 @@
 import sys
 sys.path.append('/home/user/Desktop/BridgeLabz')                     # helps in importing function from utility
-
 # importing money_change function from utility
 from Week1.Utility.utility import money_change                                        
 
-if __name__ == '__main__':                                                      
-    
-    while True:                                                                 
-        
+def min_change():
+    while True:
         try:                                                                    
             # taking user input for amount
             amount = int(input("Enter your amount in between (0 - 20000): "))   
-            if amount < 0:
+            if amount < 0 or amount > 20000:
                 continue
-            
             # calculating changes
             money_change(amount)                                                
-            
             # while loop exit 
             break                                                               
-
         except:
-            print("Enter a valid input")                                        
+            print("Enter a valid input")     
+
+if __name__ == '__main__':                                                      
+    min_change()
