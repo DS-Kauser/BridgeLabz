517726315d919ab8327a3e27338c10f0ddf9883c
DS-Kauser <kauserbest@gmail.com>
2020-01-07 10:04:29 +0530

    [Kauser]Refractor. added wind_chill_speed and main method in windchill.py

--- Week1/Function/windchill.py
+++ Week1/Function/windchill.py
@@ -1,20 +1,20 @@
 import sys
 sys.path.append('/home/user/Desktop/BridgeLabz')                     # helps in importing functioni from utility
-
 # importing cal_windchill_speed function from utility   
 from Week1.Utility.utility import cal_windchill_speed                                 
 
+def wind_chill_speed():
+    try:
+        # taking user input for temperature and velocity                                                                    
+        t = float(input("Enter temperature in Fahrenheit : "))              
+        v = float(input("Enter speed in miles per hour : "))                
+
+        # passing argument in cal_windchill_speed
+        speed_windchill = round(cal_windchill_speed(t, v), 3)
+        print("Wind chill speed : {0} degrees Fahrenheit".format(speed_windchill))
+    except:
+        print("Enter a valid input") 
+
 if __name__ == '__main__':                                                      
-    while True:                                                                 
-        try:
-            # taking user input for temperature and velocity                                                                    
-            t = float(input("Enter temperature in Fahrenheit : "))              
-            v = float(input("Enter speed in miles per hour : "))                
-
-            # passing argument in cal_windchill_speed
-            speed_windchill = round(cal_windchill_speed(t, v), 3)
-            print("Wind chill speed : {0} degrees Fahrenheit".format(speed_windchill))
-            break                                                               
-
-        except:
-            print("Enter a valid input")                                        
+    wind_chill_speed()                                                                 
+                                               
