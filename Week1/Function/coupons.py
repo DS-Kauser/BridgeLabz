77f2b8954633d39d450b3d3147096d4859644c70
DS-Kauser <kauserbest@gmail.com>
2020-01-07 09:49:21 +0530

    [Kauser]Refractor. added get_coupons and main method in coupons.py

--- Week1/Function/coupons.py
+++ Week1/Function/coupons.py
@@ -1,21 +1,19 @@
 import sys
 sys.path.append('/home/user/Desktop/BridgeLabz')                         # hepls in importing function from utility
-
 # importing coupon_number function
 from Week1.Utility.utility import gen_coupon_number                                           
 
-if __name__ == '__main__':                                                          
-    while True:                                                                     
-        try:                                                                        
-            # user-input for number of coupons required
-            num_of_coupons = int(input("Enter number of coupons you need : "))      
-            
-            # passing argument in function
-            coupons = gen_coupon_number(num_of_coupons)                                 
-            print(coupons)
-            
-            # breaking while loop
-            break                                                                   
+def get_coupons():
+    try:                                                                        
+        # user-input for number of coupons required
+        num_of_coupons = int(input("Enter number of coupons you need : "))      
         
-        except:
-            print("Enter a valid input")                                            
+        # passing argument in function
+        coupons = gen_coupon_number(num_of_coupons)                                 
+        print(coupons)                                                                   
+    except:
+        print("Enter a valid input")                                            
+
+if __name__ == '__main__':                                                          
+    get_coupons()                                                                     
+        
