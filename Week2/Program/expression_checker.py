72539e9faa0d7ac889eeb1786fdc047a7d0cca53
DS-Kauser <kauserbest@gmail.com>
2020-01-07 11:11:17 +0530

    [Kauser]Refractor. added check_exp and main method in expression_checker.py

--- Week2/Program/expression_checker.py
+++ Week2/Program/expression_checker.py
@@ -3,30 +3,21 @@ sys.path.append("/home/user/Desktop/BridgeLabz")
 
 from Week2.Utility.utility import stack
 
-if __name__ == "__main__":
-    while True:
-        try:
-            expression = input("Enter your expression : ")
-            dummy_stack = stack()
-            c = None
-            for i in expression:
-                if i == '(':
-                    dummy_stack.push(i)
-                elif i == ')':
-                    if dummy_stack.is_empty() == False:
-                        dummy_stack.pop()
-                    else:
-                        print("Expression is not balanced")
-                        c = True
-                        break
-            
-            if c == True:
-                break
-
+def check_exp():
+    dummy_stack = stack()
+    expression = input("Enter your expression : ")
+    for element in expression:
+        if element == '(':
+            dummy_stack.push(element)
+        if element == ')':
             if dummy_stack.is_empty() == True:
-                print("Expression is balanced")
-            else:
                 print("Expression is not balanced")
-            break
-        except:
-            print("Invalid input")
+                return
+            dummy_stack.pop()
+    if dummy_stack.is_empty() == True:
+        print("Expression is balanced")
+        return
+    print("Expression is not balanced")
+
+if __name__ == "__main__":
+    check_exp()
