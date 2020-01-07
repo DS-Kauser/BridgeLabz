784eaa2920ee1b467181541d3b3df115bcdcbca3
DS-Kauser <kauserbest@gmail.com>
2020-01-07 09:58:55 +0530

    [Kauser]Refractor. added is_leap_year and main method in leap_year.py

--- Week1/Function/leap_year.py
+++ Week1/Function/leap_year.py
@@ -1,21 +1,18 @@
 import sys
 sys.path.append('/home/user/Desktop/BridgeLabz')         # helps in importing function from utility
-
 # importing find_leap_year from utility
 from Week1.Utility.utility import find_leap_year                          
 
-if __name__ == '__main__':                                          
-    
-    while True:                                                     
-        try:
-            # taking year from user input                                                        
-            year = int(input("Enter year : "))                      
-            
-            # passing year as argument in find_leap_year
-            find_leap_year(year)                                    
-            
-            # breaking while loop
-            break                                                   
+def is_leap_year():
+    try:
+        # taking year from user input                                                        
+        year = int(input("Enter year : "))                      
         
-        except:
-            print("Enter Valid Input")                              
+        # passing year as argument in find_leap_year
+        find_leap_year(year)                                    
+
+    except:
+        print("Enter Valid Input")                              
+
+if __name__ == '__main__':                                          
+    is_leap_year()
