f5cb5dfce6101fc6b101ae23dbc683260f75c59d
DS-Kauser <kauserbest@gmail.com>
2020-01-07 09:29:05 +0530

    [Kauser]Refractor. added prime_anag_pelin and main method in prime_anagram_pelindrome.py

--- Week1/Algorithm/prime_anagram_pelindrome.py
+++ Week1/Algorithm/prime_anagram_pelindrome.py
@@ -6,34 +6,30 @@ from Week1.Utility.utility import find_prime
 from Week1.Utility.utility import find_pelindrome                                             
 from Week1.Utility.utility import find_anagram                                                
 
+def prime_anag_pelin():
+    try:                                                                            
+        # taking the end point and calculating prime numbers till that point
+        end = int(input("Enter the end point till you want prime number : "))       
+        prime = find_prime(end)                                                     
+        print("Prime number in between 0 to {0} : ".format(end))
+        print(prime, end = '\n\n')
+
+        # from prime number list finding pelindrome number
+        pelind = find_pelindrome(prime)                                             
+        print("Pelindrome number in these prime numbers list : ")
+        print(pelind, end = '\n\n')
+
+        # from prime number list finding anagram
+        anag = find_anagram(prime)                                                  
+        print("Anagram in these prime number list : ")
+        print(anag, end = '\n\n')
+
+        # calculating prime number which is anagram and pelindrome both
+        pelind2 = find_pelindrome(anag)                                             
+        print("Number which is pelindrome as well as anagram in prime number list : ")
+        print(pelind2)       
+    except:
+        print("Invalid Input")    
+
 if __name__ == '__main__':                                                              
-    # loop to run the code again if any is found
-    
-    while True:                                                                         
-        try:                                                                            
-            # taking the end point and calculating prime numbers till that point
-            end = int(input("Enter the end point till you want prime number : "))       
-            prime = find_prime(end)                                                     
-            print("Prime number in between 0 to {0} : ".format(end))
-            print(prime, end = '\n\n')
-
-            # from prime number list finding pelindrome number
-            pelind = find_pelindrome(prime)                                             
-            print("Pelindrome number in these prime numbers list : ")
-            print(pelind, end = '\n\n')
-
-            # from prime number list finding anagram
-            anag = find_anagram(prime)                                                  
-            print("Anagram in these prime number list : ")
-            print(anag, end = '\n\n')
-
-            # calculating prime number which is anagram and pelindrome both
-            pelind2 = find_pelindrome(anag)                                             
-            print("Number which is pelindrome as well as anagram in prime number list : ")
-            print(pelind2)
-            
-            # breaking the while loop
-            break
-        
-        except:
-            print("Invalid Input")                                                      # if any error is found
+    prime_anag_pelin()
