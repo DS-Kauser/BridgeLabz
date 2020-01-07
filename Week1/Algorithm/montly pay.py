35c46891513537668ffe75b098d5061b0342fe07
DS-Kauser <kauserbest@gmail.com>
2020-01-07 09:25:24 +0530

    [Kauser]Refractor. added payment and main method in monthly pay.py

--- Week1/Algorithm/montly pay.py
+++ Week1/Algorithm/montly pay.py
@@ -1,23 +1,21 @@
 import sys
 sys.path.append('/home/user/Desktop/BridgeLabz')                     # helps in importing the function from utility
-
 # importing monthly_payment from utility
 from Week1.Utility.utility import monthly_payment                                     
 
-if __name__ == '__main__':                                                      
-    while True:                                                                 
-        try:                                                                    
-            #taking principal, rate of interest and time period from user
-            principal = float(input("Enter Principal value : "))                
-            rate = float(input("Enter rate of interest per year : "))           
-            time_period = int(input("Enter time period : "))                    
-            
-            # using them as argument for monthly_paymentfunction
-            pay = monthly_payment(principal, rate, time_period)                 
-            print("Your Monthly payment is : Rs. {0}".format(pay))
-            
-            # breaking the loop
-            break                                                               
+def payment():
+    try:                                                                    
+        #taking principal, rate of interest and time period from user
+        principal = float(input("Enter Principal value : "))                
+        rate = float(input("Enter rate of interest per year : "))           
+        time_period = int(input("Enter time period : "))                    
+        
+        # using them as argument for monthly_paymentfunction
+        pay = monthly_payment(principal, rate, time_period)                 
+        return pay
+    except:
+        print("Please Enter a valid input")              
 
-        except:
-            print("Please Enter a valid input")                                 
+if __name__ == '__main__':                                                   
+    monthly_pay = payment()
+    print("Your monthly payment is Rs.{0}".format(monthly_pay))
