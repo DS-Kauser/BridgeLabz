fa230cb08793016e9d5b9f3b1af965eb6b661835
DS-Kauser <kauserbest@gmail.com>
2020-01-07 09:52:38 +0530

    [Kauser]Refractor. added cal_dist and main method in distance_from_origin.py

--- Week1/Function/distance_from_origin.py
+++ Week1/Function/distance_from_origin.py
@@ -1,24 +1,19 @@
 import sys
 sys.path.append('/home/user/Desktop/BridgeLabz')             # helps in importing function from utility
-
 # importing distance_from_origin function from utility
 from Week1.Utility.utility import distance_from_origin                        
 
+def cal_dist():
+    try:
+        # taking x and y value from user                                                            
+        abcissa = float(input("Enter x-value : "))                  
+        ordinate = float(input("Enter y-value: "))                  
+        
+        # passing the argument in the function to calculate distance
+        dist = round(distance_from_origin(abcissa, ordinate), 3)              
+        print(dist)
+    except:
+        print("Enter a valid input")    
+
 if __name__ == '__main__' :                                             
-    
-    while True:                                                         
-        
-        try:
-            # taking x and y value from user                                                            
-            abcissa = float(input("Enter x-value : "))                  
-            ordinate = float(input("Enter y-value: "))                  
-            
-            # passing the argument in the function to calculate distance
-            dist = round(distance_from_origin(abcissa, ordinate), 3)              
-            print(dist)
-            
-            # breaking while loop
-            break                                                       
-        
-        except:
-            print("Enter a valid input")                                
+    cal_dist()
