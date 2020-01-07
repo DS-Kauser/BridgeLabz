a636d7fc9e3ade6665cad61b785e40b46ff87a67
DS-Kauser <kauserbest@gmail.com>
2020-01-07 19:18:25 +0530

    [Kauser]Refractor. refractor stack_prime method in prime_stack.py

--- Week2/Program/prime_stack.py
+++ Week2/Program/prime_stack.py
@@ -1,21 +1,16 @@
 import sys
 sys.path.append("/home/user/Desktop/BridgeLabz")
-
-
 from Week2.Utility.utility import find_anagram
 from Week2.Utility.utility import prime
 from Week2.Utility.utility import stack
 
 def stack_prime():
-    try:
-        prime_stack = stack()
-        start = 0
-        end = 1000
-        for i in find_anagram(prime(start,end)):
-            prime_stack.push(i)
-        prime_stack.print_list()
-    except:
-        print("Check for index value")
+    prime_stack = stack()
+    start = 0
+    end = 1000
+    for i in find_anagram(prime(start,end)):
+        prime_stack.push(i)
+    prime_stack.print_list()
 
 if __name__ == "__main__":
     stack_prime()
