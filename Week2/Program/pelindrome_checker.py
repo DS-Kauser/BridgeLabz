35cc8fb7a12743012f6d0f69c60c8af7dc1e4448
DS-Kauser <kauserbest@gmail.com>
2020-01-07 18:38:52 +0530

    [Kauser]Refractor. added create_rev_deque, get_word, check_pelindrome and main method

--- Week2/Program/pelindrome_checker.py
+++ Week2/Program/pelindrome_checker.py
@@ -3,26 +3,29 @@ sys.path.append("/home/user/Desktop/BridgeLabz")
 
 from Week2.Utility.utility import deque
 
-def check_pelindrome(word):
-    try:
-        
-        deque_list = deque()
-        for i in word:
-            deque_list.add_rear(i)
+def create_rev_deque(word):
+    deque_list = deque()
+    for item in word:
+        deque_list.add_rear(item)
+    return deque_list
 
-        deque_word = ''
-        cur_node = deque_list.head
-        while cur_node is not None:
-            deque_word = deque_word + cur_node.data
-            cur_node = cur_node.next
+def get_word(deque_list):
+    deque_word = ''
+    cur_node = deque_list.head
+    while cur_node is not None:
+        deque_word = deque_word + cur_node.data
+        cur_node = cur_node.next
+    return deque_word
 
-        if word == deque_word:
-            print("Given input {0} is pelindrome".format(word))
-        else:
-            print("Given input {0} is not pelindrome".format(word))
-    except:
-        print("Invalid input")
+def check_pelindrome(word, rev_word):
+    if word == rev_word:
+        print("Given input {0} is pelindrome".format(word))
+        return 
+    print("Given input {0} is not pelindrome".format(word))
+    return
 
 if __name__ == "__main__":
     word = input("Enter a word to check : ")
-    check_pelindrome(word)
+    deque_list = create_rev_deque(word)
+    rev_word = get_word(deque_list)
+    check_pelindrome(word, rev_word)    
